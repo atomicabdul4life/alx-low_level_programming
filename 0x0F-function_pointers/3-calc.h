7bbb813bd79b78559fa37c1011c96ad447816c39
atomicabdul4life <abdullateefabdulkareem60@gmail.com>
2022-05-13 19:44:33 -0700

    3-calc.h added

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,17 @@
+#ifndef _FUNCTION_POINTERS_
+
+#define _FUNCTION_POINTERS_
+
+
+
+#include <stdlib.h>
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+op_div(int a, int b);
+op_mod(int a, int b);
+
+
+
+#endif
